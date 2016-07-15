ae387837db6bbc8d72e93e4fe5a2440bb0b2ec5f
jonatanYanovsky <leapformula4@gmail.com>
2016-07-15 11:59:01 -0400

    Add files via upload

--- Robot Controller.cpp
+++ Robot Controller.cpp
@@ -51,7 +51,7 @@ double ysonarVal = 0;
 
 
 double velocity;
-double distance; // used for velocity calculation
+
 
 
 int i=0;
@@ -60,6 +60,7 @@ double getBrakingDistance();
 void poseCallback(const nav_msgs::Odometry::ConstPtr& msg);
 void sonarCallback(const sensor_msgs::PointCloud::ConstPtr& msg2);
 void backUpIntoChargingStation();
+void exitChargingStation();
 
 int main(int argc, char** argv) {
 
@@ -77,6 +78,8 @@ int main(int argc, char** argv) {
 	// given
 
 	// 4. a new order was given: get the coffee
+
+	exitChargingStation();
 
 	// 5. call navigation stack to move the robot to face the front of the coffee terminal
 
@@ -155,6 +158,48 @@ void backUpIntoChargingStation() {
 }
 
 
+// assume we are in the charging station
+void exitChargingStation() {
+
+	ros::NodeHandle n;
+	ros::Subscriber sub = n.subscribe<nav_msgs::Odometry>("/RosAria/pose", 1, poseCallback);
+	ros::Subscriber sub2 = n.subscribe<sensor_msgs::PointCloud>("/RosAria/sonar", 1, sonarCallback);
+	ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/RosAria/cmd_vel", 100);
+	ros::Rate rate(10); // times per second to check our position
+	ros::spinOnce();
+
+	int j = 0;
+	geometry_msgs::Twist msg;
+	sensor_msgs::PointCloud msg2;
+
+	// note: don't need while(ros::ok()) since our code is linear.
+
+
+	while ((sonarDistanceValues[0] < 800) && (sonarDistanceValues[7] < 800)) {
+		msg.linear.x = 1;
+		msg.linear.y = 0;
+		msg.linear.z = 0;
+		msg.angular.x = 0; // purposely go fast to get away from the charging station 
+		msg.angular.y = 0;
+		msg.angular.z = 0;
+		pub.publish(msg);
+		rate.sleep();
+		ros::spinOnce();
+	}
+
+	msg.linear.x = 0;
+	msg.linear.y = 0;
+	msg.linear.z = 0;
+	msg.angular.x = 0; // stop
+	msg.angular.y = 0;
+	msg.angular.z = 0;
+	pub.publish(msg);
+	rate.sleep();
+	ros::spinOnce();
+
+}
+
+
 double getBrakingDistance() {
 		
 	if (velocity <= 0.75)
@@ -167,41 +212,45 @@ double getBrakingDistance() {
 
 //  to init poseCallback, set i = 0; in code and call spinOnce();
 double distanceFromLastInit; // used to see how far you have gone from the point at which "i" was set to 0
+
 void poseCallback(const nav_msgs::Odometry::ConstPtr& msg) { 
-	if (i == 0){
-	  xcurinit = msg->pose.pose.position.x;
-	  ycurinit = msg->pose.pose.position.y;
-	  thetacurinit = msg->pose.pose.orientation.z;
-	  ROS_INFO("Init values recieved: x = %f, y = %f, theta = %f", xcurinit, ycurinit, thetacurinit);
-	  
-		}
+
+	double distance; // used for velocity calculation
+
+	if (i == 0) {
+		xcurinit = msg->pose.pose.position.x;
+		ycurinit = msg->pose.pose.position.y;
+		thetacurinit = msg->pose.pose.orientation.z;
+		ROS_INFO("Init values recieved: x = %f, y = %f, theta = %f", xcurinit, ycurinit, thetacurinit);
+	}
+
 	else {
 
-	  if (i != 1) { 
-	  // if not first time running, then set the previous coordinates
-		xcurLast = xcur;
-		ycurLast = ycur;
-		thetacurLast = thetacur;
-	  }
+		if (i != 1) { 
+			// if not first time running, then set the previous coordinates
+			xcurLast = xcur;
+			ycurLast = ycur;
+			thetacurLast = thetacur;
+		}
 
-	  xcur = msg->pose.pose.position.x;
-	  ycur = msg->pose.pose.position.y;
-	  thetacur = msg->pose.pose.orientation.z; // find the new position
+		xcur = msg->pose.pose.position.x;
+		ycur = msg->pose.pose.position.y;
+		thetacur = msg->pose.pose.orientation.z; // find the new position
 
-	  // find the distance since last init
-	  distanceFromLastInit = sqrt(pow(xcur - xcurinit , 2) + pow(ycur - ycurinit , 2)); // distance formula
+		// find the distance since last init
+		distanceFromLastInit = sqrt(pow(xcur - xcurinit , 2) + pow(ycur - ycurinit , 2)); // distance formula
 
-	  xcur -= xcurinit; 
-	  ycur -= ycurinit;
-	  thetacur -= thetacurinit; // apply (0,0,0) correction
+		xcur -= xcurinit; 
+		ycur -= ycurinit;
+		thetacur -= thetacurinit; // apply (0,0,0) correction
 
-	  // calculate velocity = distance / time
-	  // distance formula
-	  distance = (double)pow((double)pow(xcur - xcurLast, 2) + (double)pow(ycur - ycurLast , 2) , 0.5); // pow(base, exponent)
+		// calculate velocity = distance / time
+		// distance formula
+		distance = (double)pow((double)pow(xcur - xcurLast, 2) + (double)pow(ycur - ycurLast , 2) , 0.5); // pow(base, exponent)
 
-	  velocity = (double)distance * 10;
+		velocity = (double)distance * 10; // 10 is ros::Rate rate(10);
 
-	  ROS_INFO("Odometer Values: x = %f, y = %f, theta = %f, velocity = %f", xcur, ycur, thetacur, velocity);
+		ROS_INFO("Odometer Values: x = %f, y = %f, theta = %f, velocity = %f", xcur, ycur, thetacur, velocity);
 
 	}
 
@@ -211,13 +260,12 @@ void poseCallback(const nav_msgs::Odometry::ConstPtr& msg) {
 
 // gets the distance that the sonar detected something at, not just (x,y) coordinates
 double sonarDistanceValues[8]; // sensors 0 to 7
+
 void sonarCallback(const sensor_msgs::PointCloud::ConstPtr& msg2) { 
 
 	for (int k=0; k<8; k++){
-		xsonarVal = msg2->points[k].x;
-		ysonarVal = msg2->points[k].y;
-		xsonarVal *= 1000;
-		ysonarVal *= 1000;
+		xsonarVal = 1000 * msg2->points[k].x;
+		ysonarVal = 1000 * msg2->points[k].y;
 		
 		// set distance of all sonar values
 		sonarDistanceValues[k] = sqrt(pow(xsonarVal , 2) + pow(ysonarVal , 2)); // pythagorean theorem
